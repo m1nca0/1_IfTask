90b2c48ed055b629080fb5439092d88826113162
m1nca0 <noname890256@gmail.com>
2026-02-20 09:37:45 +0800

    add some test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -21,5 +21,30 @@ namespace TestProject1
             var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
             Assert.That(50, Is.EqualTo(lowestMultiplication));
         }
+
+        [Test]
+        public void FirstThirdLowestTest()
+        {
+            int firstInt = 15;
+
+            int secondInt = 50;
+
+            int thirdInt = 2;
+
+            var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
+            Assert.That(30, Is.EqualTo(lowestMultiplication));
+        }
+        [Test]
+        public void SecondThirdLowestTest()
+        {
+            int firstInt = 200;
+
+            int secondInt = 50;
+
+            int thirdInt = 2;
+
+            var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
+            Assert.That(100, Is.EqualTo(lowestMultiplication));
+        }
     }
 }
