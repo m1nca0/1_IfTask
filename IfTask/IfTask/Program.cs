ad689ccbe60419e5041f2b3c9ba79a57700d0b33
m1nca0 <noname890256@gmail.com>
2026-02-20 08:30:56 +0800

    Добавьте файлы проекта.

---
+++ IfTask/IfTask/Program.cs
@@ -0,0 +1,34 @@
+﻿//#9 Составить программу нахождения произведения двух наименьших из трех различных чисел.
+using System;
+
+namespace MyProgram
+{
+    public class Logic
+    {
+        public static int Greatest(int firstInt, int secondInt, int thirdInt)
+        {
+            int[] numbers = new int[] { firstInt, secondInt, thirdInt };
+            Array.Sort(numbers);
+            return numbers[0] * numbers[1];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            int firstInt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            int secondInt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите третье число: ");
+            int thirdInt = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[] { firstInt, secondInt, thirdInt };
+            Array.Sort(numbers);
+
+            Console.WriteLine(numbers[0] * numbers[1]);
+        }
+    }
+}
