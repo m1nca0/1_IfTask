c4614bfea6f80d4d24084104d9a5dcbcea6f3eef
Mihail VASILEV <noname890256@gmail.com>
2026-03-03 23:15:00 +0800

    remade test cause i have macos

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,50 +1,47 @@
-﻿using IfTask;
-using NUnit.Framework;
-namespace TestProject1
+using IfTask;
+namespace TestProject1;
+
+public class Tests
 {
-    public class Tests
+    [SetUp]
+    public void Setup()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void FirstSecondLowestTest()
-        {
-            int firstInt = 10;
-
-            int secondInt = 5;
-
-            int thirdInt = 30;
-
-            var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
-            Assert.That(50, Is.EqualTo(lowestMultiplication));
-        }
-
-        [Test]
-        public void FirstThirdLowestTest()
-        {
-            int firstInt = 15;
-
-            int secondInt = 50;
-
-            int thirdInt = 2;
-
-            var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
-            Assert.That(30, Is.EqualTo(lowestMultiplication));
-        }
-        [Test]
-        public void SecondThirdLowestTest()
-        {
-            int firstInt = 200;
-
-            int secondInt = 50;
-
-            int thirdInt = 2;
-
-            var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
-            Assert.That(100, Is.EqualTo(lowestMultiplication));
-        }
     }
+
+    [Test]
+    public void FirstSecondLowestTest()
+    {
+        int firstInt = 10;
+
+        int secondInt = 5;
+
+        int thirdInt = 30;
+
+        var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
+        Assert.That(50, Is.EqualTo(lowestMultiplication));
+    }
+    [Test]
+    public void FirstThirdLowestTest()
+    {
+        int firstInt = 15;
+
+        int secondInt = 50;
+
+        int thirdInt = 2;
+
+        var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
+        Assert.That(30, Is.EqualTo(lowestMultiplication));
+    }
+    [Test]
+    public void SecondThirdLowestTest()
+    {
+        int firstInt = 200;
+
+        int secondInt = 50;
+
+        int thirdInt = 2;
+
+        var lowestMultiplication = Logic.Lowest(firstInt, secondInt, thirdInt);
+        Assert.That(100, Is.EqualTo(lowestMultiplication));
+    } 
 }
